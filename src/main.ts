c34d913ea7d92396929718a9ec46df6465835d48
Patryk Rossa <patryk.rossaa@gmail.com>
2025-05-22 13:14:14 +0200

    update cors policy

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({
+    origin: 'https://aether-art.netlify.app/',
+    credentials: true,
+  });
   app.use(cookieParser());
 
   // Swagger
